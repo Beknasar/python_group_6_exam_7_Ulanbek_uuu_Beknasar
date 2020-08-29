33473f8a32334e176b3746bc0e997d0c5da1e746
Beknasar <680633@gmail.com>
2020-08-29 12:08:55 +0600

    Добавлены главная страница и частичные шаблоны.

---
+++ source/webapp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Poll, Choice
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Найти")
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        exclude = []
+
+
+# class ProjectForm(forms.ModelForm):
+#     class Meta:
+#         model = Choice
+#         fields = ['name', 'description', 'date_start', 'date_end']
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+from webapp.models import Poll
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from webapp.forms import SearchForm, PollForm
+from django.db.models import Q
+
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'polls'
+    paginate_by = 5
+    paginate_orphans = 0
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        kwargs['form'] = form
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
+        data = Poll.objects.all()
+
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(Q(question__icontains=search))
+
+        return data.order_by('-date_create')
